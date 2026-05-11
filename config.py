38042bc99254c3027649ec6ddd5d2f1ce0441c02
SeiniTetsuko <136682824+SeiniTetsuko@users.noreply.github.com>
2026-05-11 22:01:20 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+{
+  "ak": "xxx",
+  "sk": "xxx",
+  "org_guid": "xxx",
+  "user_guid": "453204099452710922",
+  "owner_guid": [
+    "453204099452710922",
+    "483644689449418838"
+  ],
+  "mapping_guid": "xxxxxxxxxxxxxxxx",
+  "target_date": ""
+}
